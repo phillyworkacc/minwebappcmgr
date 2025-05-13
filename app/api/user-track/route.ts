dc517f15f0fa4e664ff1f56c30f21cc8ee0ce56d
phillyworkacc <phillyworkacc@gmail.com>
2025-05-13 11:42:02 +0100

    Update usertracking in route.ts

--- app/api/user-track/route.ts
+++ app/api/user-track/route.ts
@@ -1,39 +1,41 @@
 import { UserTrackerDB } from "@/db/UserTrackerDb";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
 const allowedOrigins = [
-   'https://minweb.freevar.com',
-   'https://detix-website.vercel.app',
+  'https://minweb.freevar.com',
+  'https://detix-website.vercel.app',
 ];
 
-export async function POST (req: NextApiRequest, res: NextApiResponse) {
-   const origin = req.headers.origin;
+export async function POST (req: NextRequest) {
+   const origin = req.headers.get('origin');
 
+   const headers = new Headers();
    if (origin && allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-      res.setHeader('Vary', 'Origin');
+      headers.set('Access-Control-Allow-Origin', origin);
+      headers.set('Vary', 'Origin');
    }
 
-   const body = await req.body.json() as UserTrackingDataPartial;
-   const authHeader = req.headers.authorization;
+   const body = await req.json() as UserTrackingDataPartial;
+   const authHeader = await req.headers.get("authorization")
 
    const { location, time, utmsource, device, os, page } = body;
 
    if (authHeader == undefined) {
       //! invalid request
-      res.status(500).json({ message: "Forbidden Request" })
+      return NextResponse.json({ message: "Forbidden Request" }, { status: 500, headers })
 
    } else if (page == undefined || location == undefined || os == undefined || time == undefined || utmsource == undefined || device == undefined) {
       //! invalid request
-      res.status(500).json({ message: "Forbidden Request" })
+      return NextResponse.json({ message: "Forbidden Request" }, { status: 500, headers })
 
    } else if (authHeader === "lSjFmFulZGPw+So4SVI5W/jcd3RMm6YH/c5nS2cGBWQ=") {
       //* valid request
       try {
          await UserTrackerDB.insert({ time, location, utmsource, device, os, page })
-         res.status(200).json({ message: "success" })
+         return NextResponse.json({ message: "success" }, { status: 200, headers })
       } catch (er) {
-         res.status(500).json({})
+         return NextResponse.json({}, { status: 500, headers })
       }
    }
+
 }
