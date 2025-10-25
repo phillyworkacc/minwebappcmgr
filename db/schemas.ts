ba7c65e12abd5c4242a739e8346259dc060f48a9
AyomiposiPhilip <ayomiposi.opadijo@gmail.com>
2025-10-25 02:22:07 +0100

    update ui v5

--- db/schemas.ts
+++ db/schemas.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
    id: serial("id").primaryKey(),
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 if (!process.env.NEON_DB) {
-   throw new Error("No process.env.DATABASE_URL");
+   throw new Error("No process.env.NEON_DB");
 }
 
 export default defineConfig({
